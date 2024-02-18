8f1b6098c6747702d6152744395ba6192a1ef52f
Oliviu <dienes.oliviu@gmail.com>
2024-02-18 21:12:09 +0200

    Added a header component and search-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlantCardComponent } from "./shared/plant-card/plant-card.component";
+import { HeaderComponent } from "./core/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlantCardComponent],
+  imports: [RouterOutlet, PlantCardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
