07abd656b6262b839b3c48cf62f449b3d04a9f6f
Oliviu <dienes.oliviu@gmail.com>
2024-02-18 23:11:27 +0200

    Added a sidebar skeleton to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlantCardComponent } from "./shared/plant-card/plant-card.component";
 import { HeaderComponent } from "./core/header/header.component";
+import { SideBarComponent } from "./core/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlantCardComponent, HeaderComponent],
+  imports: [RouterOutlet, PlantCardComponent, HeaderComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
